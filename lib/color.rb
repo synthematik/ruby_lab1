6f5bb657492f4b7b0f8423f483cdba65da519666
Garik <garfild_2003@mail.ru>
2024-03-05 23:31:46 +0300

    Finished task1

--- lib/color.rb
+++ lib/color.rb
@@ -2,6 +2,10 @@
 # @param [Integer] g
 # @param [Integer] b
 def color(r, g, b)
-  # 100.to_s(16) => 64
-  nil
+  r = r.clamp(0, 255)
+  g = g.clamp(0, 255)
+  b = b.clamp(0, 255)
+
+  str = (r.to_s(16) + g.to_s(16) + b.to_s(16)).upcase
+  str.gsub('0', '00')
 end
