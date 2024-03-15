8518efa917ed2a2d168adae866f90c23f9aacc10
Garik <garfild_2003@mail.ru>
2024-03-15 19:03:40 +0300

    Finished task 3

--- lib/sequence.rb
+++ lib/sequence.rb
@@ -1,5 +1,21 @@
 # @param [String] s
 def sequence(s)
-  s.downcase.scan(/((.)\2*)/).map { |match| match[0].length }.max.to_i
+  return 0 if s.empty?
+
+  max_length = 0
+  counter = 1
+  s = s.downcase
+
+  (1...s.length).each do |i|
+    if s[i] == s[i - 1]
+      counter += 1
+    else
+      max_length = [max_length, counter].max
+      counter = 1
+    end
+  end
+
+  max_length = [max_length, counter].max
 end
 
+puts sequence('')
